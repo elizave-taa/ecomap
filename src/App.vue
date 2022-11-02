03e78e649c0b8f0208b72cd3af41502427fdcc91
Elizaveta <chueva@sfedu.ru>
2022-11-02 17:27:28 +0300

    Assemble a Welcome Page from components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,106 @@
-<script setup>
+<script>
 
-import HelloWorld from './components/HelloWorld.vue'
+import GlobalHeader from "/src/components/GlobalHeader.vue";
+import ButtonGeneral from "/src/components/ButtonGeneral.vue";
+import AccountLogIn from "/src/components/AccountLogIn.vue";
+
+export default {
+  components: { GlobalHeader, ButtonGeneral, AccountLogIn }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <global-header />
+
+    <div class="parent">
+
+      <div class="login-form">
+
+          <div><account-log-in/></div>
+          <div class="text-or">ИЛИ</div>
+          <div class="login-button">
+            <button-general variant="limpid">
+              <span>Продолжить без входа</span>
+            </button-general>
+          </div>
+      </div>
+
+      <div class ="second-container">
+      <div class="description">
+        <div class="in-description">
+          <h1>EcoMap</h1>
+          <h2>Наши усилия и действия на благо природы</h2>
+        </div>
+      </div>
+      </div>
+    </div>
+
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+
+  <div class = "description"></div>
+  <!--  <RouterView />-->
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+.app {
+  background-image: url('/src/assets/map.png');
+  background-size: cover;
+  background-position: center;
+  width: 100vw;
+  height: 100vh;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+span{
+  padding: 30px 68px 30px 68px;
+  font-size: 22px;
+  line-height: 50px;
+  font-weight: 100;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.text-or{
+  color: white;
+  font-weight: 300;
+  font-size : 22px;
 }
+
+.parent {
+  margin-top: 10%;
+  margin-left: 0;
+  font-family: Inter, sans-serif;
+  justify-content: space-around;
+  display: flex;
+}
+h1{
+  font-size: 75px;
+  font-weight: 200;
+}
+h2{
+  font-size: 25px;
+  font-style: italic;
+  font-weight: 300;
+}
+
+.in-description{
+  padding: 2px 20px 1px 20px;
+  text-align: right;
+}
+
+.description{
+
+  border-right: white solid 4px;
+  text-align: center;
+  line-height: 20px;
+  color: white;
+}
+
+
+.login-form div{
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+
 </style>
