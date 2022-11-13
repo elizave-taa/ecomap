5bf0d82d6a6d1c5c942757aa291fa27267ac7f38
ana_sta_r <anastasiastar0611@gmail.com>
2022-11-13 23:00:09 +0300

    Add new route for registration

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import {createRouter, createWebHistory} from "vue-router";
-
+import RegistrationPage from "../views/RegistrationPage.vue";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {
+            path: '/registration',
+            name: 'registration',
+            component: RegistrationPage
+        },
     ]
 })
 
