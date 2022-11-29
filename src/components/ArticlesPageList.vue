1c423579a7b2b5aae53c5067ab59b9ecd9333ced
ana_sta_r <anastasiastar0611@gmail.com>
2022-11-29 19:15:00 +0300

    Use and style paginator

--- src/components/ArticlesPageList.vue
+++ src/components/ArticlesPageList.vue
@@ -1,8 +1,10 @@
 <script>
 import ArticleCard from "./ArticleCard.vue";
+import ArticlesPaginator from "./ArticlesPaginator.vue";
 export default {
   components: {
-    ArticleCard
+    ArticleCard,
+    ArticlesPaginator,
   },
   data() {
     return {}
@@ -23,6 +25,9 @@ export default {
     <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
     <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
   </div>
+  <div>
+    <articles-paginator class="ap-paginator" :page-count="10" next-text=">" prev-text="<" />
+  </div>
 </template>
 
 <style scoped>
@@ -31,5 +36,35 @@ export default {
   grid-template-columns: 1fr 1fr 1fr;
   column-gap: 80px;
   row-gap: 50px;
+}
+
+.ap-paginator {
+  list-style: none;
+  display: flex;
+  column-gap: 15px;
+  justify-content: center;
+  margin-top: 50px;
+  padding-left: 0;
+}
+
+.ap-paginator:deep(li) {
+  display: block;
+  background-color: #859F8A;
+  border-radius: 5px;
+}
+
+.ap-paginator:deep(a) {
+  display: block;
+  color: #fff;
+  padding: 5px 10px;
+}
+
+.ap-paginator:deep(li.active) {
+  background-color: #3D6144;
+}
+
+.ap-paginator:deep(li.disabled a) {
+  text-decoration: none !important;
+  cursor: default !important;
 }
 </style>
