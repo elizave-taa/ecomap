9f9b9f13b8ff340782e51fe979fed56cf99336f8
ana_sta_r <anastasiastar0611@gmail.com>
2022-11-29 18:05:40 +0300

    Add new component ArticlesPageList

---
+++ src/components/ArticlesPageList.vue
@@ -0,0 +1,35 @@
+<script>
+import ArticleCard from "./ArticleCard.vue";
+export default {
+  components: {
+    ArticleCard
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<template>
+  <div class="ap-grid">
+    <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
+    <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
+    <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
+    <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
+    <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
+    <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
+    <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
+    <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
+    <article-card title="Экология человека и экологическая культура" :likes="8" image="https://pic.xenomorph.ru/2013-04/1365242838_16.jpg"/>
+  </div>
+</template>
+
+<style scoped>
+.ap-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 80px;
+  row-gap: 50px;
+}
+</style>
