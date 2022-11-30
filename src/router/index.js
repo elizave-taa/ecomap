e2986a1eb90b54d70104adef7786aa7841650c7f
Elizaveta <chueva@sfedu.ru>
2022-11-30 19:36:55 +0300

    Add Route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router";
 import WelcomePage from "../views/WelcomePage.vue";
+import ProfilePage from "../views/ProfilePage.vue";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -7,6 +8,11 @@ const router = createRouter({
             name: 'WelcomePage',
             component: WelcomePage,
             path: '/'
+        },
+        {
+            name: 'ProfilePage',
+            component: ProfilePage,
+            path: '/profile'
         }
     ]
 })
