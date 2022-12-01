7b438ac78af3a3abf1d5330a31ea62b1b767ab47
ana_sta_r <anastasiastar0611@gmail.com>
2022-12-01 21:54:43 +0300

    Wrap ArticlesPage in BContainer

--- src/views/ArticlesPage.vue
+++ src/views/ArticlesPage.vue
@@ -1,10 +1,12 @@
 <script>
 import ArticlesPageHeader from "../components/ArticlesPageHeader.vue";
 import ArticlesPageList from "../components/ArticlesPageList.vue";
+import {BContainer} from "bootstrap-vue";
 export default {
   components: {
     ArticlesPageHeader,
     ArticlesPageList,
+    BContainer,
   },
   data() {
     return {}
@@ -14,12 +16,18 @@ export default {
 </script>
 
 <template>
-  <articles-page-header class="ap-header"/>
-  <articles-page-list/>
+  <b-container class="ap-container">
+    <articles-page-header class="ap-header"/>
+    <articles-page-list/>
+  </b-container>
 </template>
 
 <style scoped>
 .ap-header {
   margin-bottom: 40px;
+}
+
+.ap-container {
+  padding: 30px 0;
 }
 </style>
