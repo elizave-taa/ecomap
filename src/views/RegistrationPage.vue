de4e4d4ec8d9741933c4858dae8939be8bcf56ae
ana_sta_r <anastasiastar0611@gmail.com>
2022-12-01 22:30:50 +0300

    Make logic for back button

--- src/views/RegistrationPage.vue
+++ src/views/RegistrationPage.vue
@@ -54,7 +54,7 @@ export default {
     <b-container>
       <div class="reg-page">
         <div class="top-menu">
-          <button class="back-btn">
+          <button class="back-btn" @click="$router.go(-1)">
             <svg width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M15.125 21.1L6.7 12.7q-.15-.15-.212-.325q-.063-.175-.063-.375t.063-.375q.062-.175.212-.325l8.425-8.425q.35-.35.875-.35t.9.375q.375.375.375.875t-.375.875L9.55 12l7.35 7.35q.35.35.35.862q0 .513-.375.888t-.875.375q-.5 0-.875-.375Z"/></svg>
             Назад
           </button>
