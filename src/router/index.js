8c36d86ef7686e0ecd3670bc983000b9cec338fe
Elizaveta <chueva@sfedu.ru>
2022-12-02 19:05:53 +0300

    Add routers

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import ArticlesPage from "../views/ArticlesPage.vue";
 import WelcomePage from "/src/views/WelcomePage.vue";
 import RegistrationPage from "../views/RegistrationPage.vue";
 import ArticleView from "../views/ArticleView.vue";
+import MapPage from "../views/MapPage.vue";
+import DataPoint from "../views/DataPoint.vue";
 
 const router = createRouter({
     history: createWebHistory("/ecomap/"),
@@ -28,6 +30,17 @@ const router = createRouter({
             component: ArticleView,
             props: true,
         },
+        {
+            path: '/map',
+            name: 'map-page',
+            component: MapPage
+        },
+        {
+            path: '/point',
+            name: 'new-point',
+            component: DataPoint
+        }
+
     ]
 })
 
