e19785f29c8d8031924c5be4e288c70b8e14ec14
Elizaveta <chueva@sfedu.ru>
2022-12-02 17:25:20 +0300

    Add Api Yandex map

--- src/views/MapPage.vue
+++ src/views/MapPage.vue
@@ -2,22 +2,35 @@
 import GlobalHeader from "/src/components/GlobalHeader.vue";
 import KindsOfTrash from "/src/components/KindsOfTrash.vue";
 import ButtonGeneral from "/src/components/ButtonGeneral.vue";
-import { yandexMap, ymapMarker } from 'vue-yandex-maps'
+import { yandexMap } from 'vue-yandex-maps'
 import {BFormCheckbox} from "bootstrap-vue";
 export default {
-  components: {GlobalHeader, KindsOfTrash, ButtonGeneral, BFormCheckbox, yandexMap, ymapMarker },
+  components: {GlobalHeader, KindsOfTrash, ButtonGeneral, BFormCheckbox, yandexMap},
   data() {
     return {
       checked1: false,
-      checked2: false
+      checked2: false,
+      settings: {
+        apiKey: '2b56651c-9a27-46cb-8ec3-aaa9f5771ca2', // Индивидуальный ключ API
+        lang: 'ru_RU', // Используемый язык
+        coordorder: 'latlong', // Порядок задания географических координат
+        debug: false, // Режим отладки
+        version: '2.1' // Версия Я.Карт
+      }
     }
+  },
+  created() {
+    // Установить скрипты для использования яндекс карты
+    let scriptYandexMap = document.createElement('script');
+    scriptYandexMap.setAttribute('src', 'https://api-maps.yandex.ru/2.1/?lang=ru_RU');
+    document.head.appendChild(scriptYandexMap);
   }
 }
 </script>
 
 <template>
+  <global-header class="head"/>
   <div class="map-page">
-    <global-header class="head"/>
 
     <div class="left-nav">
       <div class="choose-kind">
@@ -40,16 +53,23 @@ export default {
         </button-general>
       </div>
     </div>
+    <div class="map">
+      <yandex-map class="ymap" :settings="settings" :coords="[47.24, 39.73]" :zoom="12">
+      </yandex-map>
+    </div>
   </div>
-
 </template>
 
 <style scoped>
 
+.map-page{
+  width: 100vw;
+  height: 94vh;
+  display: flex;
+}
 .left-nav {
-  color: #341A0C;
   font-family: Inter, sans-serif;
-  width: 23%;
+  width: 25%;
   height: 91vh;
   display: flex;
   flex-direction: column;
@@ -94,5 +114,15 @@ span {
 
 .head {
   box-shadow: 0 3px 2px rgba(0, 0, 0, 0.25);
+}
+
+.map{
+  width: 100%;
+  height: 100%;
+  background-color: #981111;
+}
+.ymap{
+  width: 100%;
+  height: 100%;
 }
 </style>
