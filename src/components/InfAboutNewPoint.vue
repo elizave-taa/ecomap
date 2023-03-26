4e52351fe55aa70bcdc33e1edbec5854bfc3aee4
ana_sta_r <anastasiastar0611@gmail.com>
2023-03-26 21:53:48 +0300

    Refactor point form

--- src/components/InfAboutNewPoint.vue
+++ src/components/InfAboutNewPoint.vue
@@ -1,10 +1,31 @@
 <script>
-import {BFormInput, BFormGroup, BFormCheckbox, BFormTextarea, BAvatarGroup, BAvatar} from "bootstrap-vue"
+import {
+  BFormInput,
+  BFormGroup,
+  BFormCheckbox,
+  BFormTextarea,
+  BAvatarGroup,
+  BAvatar,
+  BContainer,
+  BRow,
+  BCol
+} from "bootstrap-vue"
 import ButtonGeneral from "./ButtonGeneral.vue";
 
 export default {
   name: "EnterInformation",
-  components: {ButtonGeneral, BFormInput, BFormGroup, BFormCheckbox, BFormTextarea, BAvatarGroup, BAvatar },
+  components: {
+    ButtonGeneral,
+    BFormInput,
+    BFormGroup,
+    BFormCheckbox,
+    BFormTextarea,
+    BAvatarGroup,
+    BAvatar,
+    BRow,
+    BCol,
+    BContainer
+  },
   data() {
     return {
       address: '',
@@ -12,11 +33,11 @@ export default {
       comment: '',
       selected: [], // Must be an array reference!
       options: [
-        { text: 'Бумага', value: 'бумага' },
-        { text: 'Стекло', value: 'стекло' },
-        { text: 'Пластик', value: 'пластик' },
-        { text: 'Одежда', value: 'одежда' },
-        { text: 'Тетра Пак', value: 'тетра пак' }
+        {text: 'Бумага', value: 'бумага'},
+        {text: 'Стекло', value: 'стекло'},
+        {text: 'Пластик', value: 'пластик'},
+        {text: 'Одежда', value: 'одежда'},
+        {text: 'Тетра Пак', value: 'тетра пак'}
       ]
     }
   }
@@ -24,144 +45,138 @@ export default {
 </script>
 
 <template>
-  <div class="parts">
-    <div class="left-part">
-      <div class="head-line">Введите информацию о точке</div>
-
-      <div class="line">
-        <span class="point">Адрес</span>
-        <b-form-input class="input" v-model="address"></b-form-input>
-      </div>
-
-      <span class="point">Виды мусора</span>
-      <b-form-group
-          v-slot="{ ariaDescribedby }"
-      >
-        <b-form-checkbox
-            v-for="option in options"
-            v-model="selected"
-            :key="option.value"
-            :value="option.value"
-            :aria-describedby="ariaDescribedby"
-            name="flavour-3a"
-            class="check-box"
-        >
-          {{ option.text }}
-        </b-form-checkbox>
-      </b-form-group>
-
-      <div class="line">
-        <span class="point">Другие</span>
-        <b-form-input class="input" v-model="others"></b-form-input>
-      </div>
-
-      <div class="line">
-        <span class="point">Комментарий</span>
-        <b-form-textarea
-            class="input-comment"
-            id="textarea-default"
-        ></b-form-textarea>
-      </div>
-    </div>
-
-    <!--Здесь картинки-->
-    <div class="right-part">
-      <div>
-        <div class="head-line">Добавьте фотографии</div>
-        <div >
-          <b-avatar
-              size="4rem"
-              rounded="sm"
-              class="line-image"
-              src="/src/assets/add_photo.svg">
-          </b-avatar>
-          <b-avatar
-              size="4rem"
-              rounded="sm"
-              class="line-image"
-              src="/src/assets/add_photo.svg">
-          </b-avatar>
-          <b-avatar
-              size="4rem"
-              rounded="sm"
-              class="line-image"
-              src="/src/assets/add_photo.svg">
-          </b-avatar>
-          <b-avatar
-              size="4rem"
-              rounded="sm"
-              class="line-image"
-              src="/src/assets/add_photo.svg">
-          </b-avatar>
-        </div>
-      </div>
-
-      <div>
-        <div class="text-ecomap">
-          Спасибо за вашу помощь! Мы рассмотрим вашу точку и пришлем ответ в течение 3 дней.
-          С любовью, EcoMap &#10084;
-        </div>
-        <button-general variant="basic">
-          <span class="button-text">Предложить свою точку</span>
-        </button-general>
-      </div>
-
-    </div>
-
+  <div>
+    <b-container>
+      <b-row>
+        <b-col cols="12" class="inf-col">
+          <div class="head-line">Введите информацию о точке</div>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="6" class="inf-col">
+          <div class="line">
+            <span class="point">Адрес</span>
+            <b-form-input class="input" v-model="address"></b-form-input>
+          </div>
+          <div>
+            <span class="point">Виды мусора</span>
+            <b-form-group
+                v-slot="{ ariaDescribedby }"
+            >
+              <b-form-checkbox
+                  v-for="option in options"
+                  v-model="selected"
+                  :key="option.value"
+                  :value="option.value"
+                  :aria-describedby="ariaDescribedby"
+                  name="flavour-3a"
+                  class="check-box"
+              >
+                {{ option.text }}
+              </b-form-checkbox>
+            </b-form-group>
+          </div>
+          <div class="line">
+            <span class="point">Комментарий</span>
+            <b-form-textarea
+                class="input-comment"
+                id="textarea-default"
+            ></b-form-textarea>
+          </div>
+        </b-col>
+        <b-col md="6" class="inf-col inf-col-flex">
+          <div>
+            <div class="head-line">Добавьте фотографии</div>
+            <div>
+              <b-avatar
+                  size="4rem"
+                  rounded="sm"
+                  class="line-image"
+                  src="/src/assets/add_photo.svg">
+              </b-avatar>
+              <b-avatar
+                  size="4rem"
+                  rounded="sm"
+                  class="line-image"
+                  src="/src/assets/add_photo.svg">
+              </b-avatar>
+              <b-avatar
+                  size="4rem"
+                  rounded="sm"
+                  class="line-image"
+                  src="/src/assets/add_photo.svg">
+              </b-avatar>
+              <b-avatar
+                  size="4rem"
+                  rounded="sm"
+                  class="line-image"
+                  src="/src/assets/add_photo.svg">
+              </b-avatar>
+            </div>
+          </div>
+          <div class="text-ecomap">
+            Спасибо за вашу помощь! Мы рассмотрим вашу точку и пришлем ответ в течение 3 дней.
+            С любовью, EcoMap &#10084;
+          </div>
+          <div class="to-right-bottom">
+            <button-general class="inf-button">
+              Предложить свою точку
+            </button-general>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <style scoped>
-
-.parts{
-  font-family: Inter, sans-serif;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: rgba(82, 115, 89, 0.75);
-  border-radius: 8px;
-}
-
-.left-part{
-  width: 45%;
-  height: 80%;
+.inf-col-flex {
+  min-height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
 }
 
-.right-part{
-  width: 40%;
-  height: 80%;
+.inf-button {
+  padding: 12px 22px;
+  font-size: 18px;
+  font-weight: 500;
+}
+
+.inf-col {
+  padding: 0 50px;
+}
+
+.to-right-bottom {
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  justify-content: flex-end;
+  align-items: flex-end;
+  margin-top: auto;
 }
 
-.head-line{
+.head-line {
   color: #ffffff;
   font-size: 25px;
   font-weight: 400;
   margin-bottom: 20px;
 }
 
-.point{
+.point {
   font-size: 16px;
   color: #ffffff;
   line-height: 35px;
 }
 
 .input {
-  color: #ffffff!important;
+  color: #ffffff !important;
   background: none;
   width: 65%;
-  font-size: 16px!important;
+  font-size: 16px !important;
   height: 30px;
   margin-bottom: 10px;
 }
 
 .input-comment {
-  color: #ffffff!important;
+  color: #ffffff !important;
   background: none;
   width: 65%;
 }
@@ -178,41 +193,36 @@ export default {
   box-shadow: none;
 }
 
-.button-text{
-  line-height: 35px;
-  font-size: 20px;
-  padding: 30px;
-}
-
-.line{
+.line {
   display: flex;
   justify-content: space-between;
 }
 
-.line-image{
+.line-image {
   margin-right: 25px;
   padding: 20px;
-  background-color: rgba(133, 159, 138)!important;
+  background-color: rgb(var(--c-primary-light-rgb)) !important;
 }
 
-.text-ecomap{
+.text-ecomap {
   color: #ffffff;
   font-size: 15px;
   width: 90%;
   margin-bottom: 20px;
+  margin-top: 20px;
 }
 
-.line-image:hover{
+.line-image:hover {
   box-shadow: 2px 2px #523F34;
   cursor: pointer;
 }
 
-.line-image:deep(button){
-  background-color: white;
+.line-image:deep(button) {
+  background-color: white !important;
   padding: 30px;
 }
 
-.check-box:deep(label){
+.check-box:deep(label) {
   color: #ffffff;
 }
 </style>
