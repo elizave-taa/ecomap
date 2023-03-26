43ff8604f090d0fb543b1057188fd0727d3c3d2a
Elizaveta <chueva@sfedu.ru>
2023-03-26 19:59:22 +0300

    Change Buttons

--- src/views/MapPage.vue
+++ src/views/MapPage.vue
@@ -50,7 +50,7 @@ export default {
     </div>
     <div class="map">
       <yandex-map class="ymap" :settings="settings" :coords="[47.24, 39.73]" zoom="12.4">
-        +++<my-component slot="balloon"></my-component>
+        <my-component slot="balloon"></my-component>
       </yandex-map>
     </div>
   </div>
