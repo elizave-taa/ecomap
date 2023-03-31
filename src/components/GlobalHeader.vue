333a9b92e8b7a5f872d262fd3be1708f4443fc66
Elizaveta <chueva@sfedu.ru>
2023-04-01 02:09:35 +0300

    Ensure the adaptability Global Header

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,45 +1,54 @@
 <script>
-import {BDropdown, BDropdownItem} from "bootstrap-vue"
+import {BDropdown, BDropdownItem, BContainer} from "bootstrap-vue"
 import {RouterLink} from "vue-router"
 export default {
-  components: {BDropdown, BDropdownItem, RouterLink}
+  components: {BDropdown, BDropdownItem, RouterLink, BContainer}
 }
 </script>
 
 <template>
   <div class="global-header">
+    <b-container class="main">
     <img alt="ecomap" class="logo" src="/src/assets/logo.svg" />
     <router-link class="link" to="/we">О проекте</router-link>
     <router-link class="link" to="/articles">Статьи про экологию</router-link>
     <router-link class="link" to="/map">Смотреть карту</router-link>
-    <b-dropdown id="dropdown-right" right text="Контакты" variant="primary" class="contacts">
-      <b-dropdown-item href="#">Как с нами связаться?</b-dropdown-item>
-      <hr/>
-      <b-dropdown-item href="#">Контакт 1</b-dropdown-item>
-      <b-dropdown-item href="#">Контакт 2</b-dropdown-item>
-    </b-dropdown>
-    <img alt="профиль" class="logo-user" src="/src/assets/user.svg" />
+    <router-link class="link" to="/profile">Мой профиль</router-link>
+
+      <img alt="карта" class="alt-logo" src="/src/assets/map-page.svg" />
+      <img alt="о нас" class="alt-logo" src="/src/assets/about-us.svg" />
+      <img alt="статьи" class="alt-logo" src="/src/assets/articles.svg" />
+      <img alt="профиль" class="logo-user" src="/src/assets/user.svg" />
+    </b-container>
   </div>
+
 </template>
 
 <style scoped>
 .global-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 6px 150px;
+  padding: 6px;
   background-color: rgba(82, 115, 89, 0.75);
   position: relative;
   z-index: 10;
+}
+
+.main{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  z-index: 10;
+  width: 100%!important;
 }
 .logo {
   width: 25px;
   height: 23px;
 }
 .logo-user {
-  cursor: pointer;
-  width: 20px;
-  height: 20px;
+  display: none;
+}
+.alt-logo{
+  display: none;
 }
 .link {
   font-family: Inter, sans-serif;
@@ -48,11 +57,28 @@ export default {
   text-decoration: none;
   color: #fff !important;
 }
-.contacts:deep(button){
-  padding: 0;
-  background: none!important;
-  border: none!important;
-  font-family: Inter, sans-serif;
-  font-size: 15px;
+@media (max-width: 1050px) {
+  .global-header {
+    height: 100%;
+    background-color: rgba(82, 115, 89, 0.75);
+    position: relative;
+    z-index: 10;
+  }
+  .link{
+    display: none;
+  }
+  .logo-user {
+    display: flex;
+    width: 25px;
+    height: 25px;
+  }
+  .alt-logo{
+    display: flex;
+    width: 25px;
+    height: 25px;
+  }
+  .logo{
+    display: none;
+  }
 }
 </style>
