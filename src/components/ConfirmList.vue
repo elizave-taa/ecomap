391459d2528aebf7a8c199ef16eea567882367c2
ana_sta_r <anastasiastar0611@gmail.com>
2023-04-01 19:10:20 +0300

    Add ability to accept and delete points

--- src/components/ConfirmList.vue
+++ src/components/ConfirmList.vue
@@ -1,16 +1,130 @@
 <script>
+import axios from "axios";
+import {BAvatar} from "bootstrap-vue";
+import TypeBadge from "./TypeBadge.vue"
+import ButtonGeneral from "./ButtonGeneral.vue";
+
 export default {
-  data() {
-    return {}
+  components: {
+    BAvatar,
+    TypeBadge,
+    ButtonGeneral,
   },
-  methods: {},
+  data() {
+    return {
+      points: [],
+    }
+  },
+  methods: {
+    getPoints() {
+      axios.get("http://80.90.190.25:5243/api/map", {
+        headers: {
+          "authorization": "daa1321becebd9767f1b9bee75506c5b0b6190e029c1bf203654db830b8b7d55"
+        },
+        params: {
+          isAccepted: false,
+          allIncludes: false,
+          types: JSON.stringify([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]),
+          x1: 0,
+          x2: 0,
+          y1: 0,
+          y2: 0,
+        }
+      }).then((response) => {
+        this.points = response.data.points;
+      })
+    },
+
+    deletePoint(id) {
+      axios.delete(`http://80.90.190.25:5243/api/map/${id}`, {
+        headers: {
+          "authorization": "daa1321becebd9767f1b9bee75506c5b0b6190e029c1bf203654db830b8b7d55"
+        },
+      }).then(() => {
+        this.getPoints()
+      })
+    },
+
+    acceptPoint(point) {
+      point.isAccepted = true;
+      axios.put(`http://80.90.190.25:5243/api/map/${point.id}`, point, {
+        headers: {
+          "authorization": "daa1321becebd9767f1b9bee75506c5b0b6190e029c1bf203654db830b8b7d55"
+        },
+      }).then(() => {
+        this.getPoints()
+      })
+    },
+  },
+
+  mounted() {
+    this.getPoints()
+  }
 }
 </script>
 
 <template>
-
+  <div v-for="point in points" class="cl-point">
+    <h5>
+      {{ point.title }}
+    </h5>
+    <h6>
+      {{ point.address }}
+    </h6>
+    <div class="cl-icons">
+      <type-badge v-for="type in JSON.parse(point.types)" :point-id="point.id" :type="type"/>
+    </div>
+    <div>
+      <b-avatar
+          v-for="image in point.images"
+          size="5rem"
+          rounded="sm"
+          class="cl-image"
+          :src="image">
+      </b-avatar>
+    </div>
+    <p class="cl-comment">
+      Комментарий: {{point.comment}}
+    </p>
+    <div class="cl-actions">
+      <button-general class="cl-button" @click="acceptPoint(point)">Принять</button-general>
+      <button-general class="cl-button" variant="red" @click="deletePoint(point.id)">Отклонить</button-general>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.cl-point {
+  background-color: rgb(var(--c-primary-rgb));
+  border-radius: 8px;
+  color: #fff;
+  margin-bottom: 15px;
+  padding: 15px;
+}
 
+.cl-image {
+  margin-right: 10px;
+}
+
+.cl-comment {
+  margin-top: 5px;
+  margin-bottom: 0;
+}
+
+.cl-icons {
+  display: flex;
+  gap: 8px;
+  flex-wrap: wrap;
+  margin-bottom: 10px;
+}
+
+.cl-actions {
+  display: flex;
+  justify-content: flex-end;
+  gap: 15px;
+}
+
+.cl-button {
+  padding: 8px 20px;
+}
 </style>
