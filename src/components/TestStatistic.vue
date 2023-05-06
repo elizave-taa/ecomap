bbd60b25df481d862e50db43b894abe4ccc2055f
Elizaveta <chueva@sfedu.ru>
2023-05-06 14:05:42 +0300

    Add tests about ecology

--- src/components/TestStatistic.vue
+++ src/components/TestStatistic.vue
@@ -1,20 +1,62 @@
 <script>
+import tests from "/src/helpers/tests.js";
+import Test from "./Test.vue";
+import {useUserStore} from "../PiniaStore.js";
+import {ModalPlugin, BModal, BButton } from 'bootstrap-vue'
 export default {
+  components: { BModal, ModalPlugin, BButton, Test },
   name: "TestStatistic",
+  methods: {
+    showModal() {
+      this.$refs['modal-1'].show();
+    },
+  },
   data() {
     return {
-      count: 0
+      count: 0,
+      tests: tests,
+      pickedTest: null,
+      userStore: useUserStore()
     }
-  }
+  },
+  computed: {
+    user() {
+      return this.userStore.user
+    },
+    userName(){ return this.user?.name}
+  },
 }
 </script>
 
 <template>
   <div class="point-num">
       <div class="description">
-        <div class ="text">Пройдено тестов:</div>
-        <img src="/src/assets/tests.svg" class="image">
-        <div class="count"> {{count}}/3 </div>
+        <div class ="text">Пройдено тестов: </div>
+        <div>
+          <b-button class="btn" @click="showModal()">
+            <img src="/src/assets/tests.svg" class="image">
+            <div class="count"> {{count}}/3</div>
+          </b-button>
+        </div>
+        <b-modal ref="modal-1"  size="lg" centred hide-footer hide-header class="modal-window">
+          <div v-if="!pickedTest">
+            <div class="image-container">
+              <img alt="ecomap" class="logo" src="/src/assets/test.jpg" />
+              <div class="centered"><div class="head">Здравствуйте, {{userName}}!</div>Давайте вместе будем учиться и узнавать новое</div>
+            </div>
+            <div class="head">Cписок тестов</div>
+            <button class="test-list" v-for="test in tests" style="margin-right: 10px;" @click="pickedTest = test">
+              {{test.title}}
+            </button>
+          </div>
+          <div v-if="pickedTest">
+            <Test
+                :key="pickedTest.id"
+                :test-data="pickedTest"
+                @close="pickedTest = null"
+            />
+          </div>
+        </b-modal>
       </div>
     </div>
 </template>
@@ -30,11 +72,8 @@ export default {
   flex-direction: column;
   justify-content: center;
 }
-
-
-
 .description{
-  height: 80%;
+  height: 70%;
   font-size: 15px;
   font-weight: 500;
   text-align: center;
@@ -44,22 +83,85 @@ export default {
   flex-direction: column;
   align-items: center;
 }
-
+.logo{
+  width: 100%;
+}
+.btn,
+.btn:hover,
+.btn:focus{
+  background: none!important;
+  border: none!important;
+  box-shadow: none!important;
+}
+.image-container {
+  width: 100%;
+  position: relative;
+  text-align: center;
+  color: white;
+  margin-bottom: 10px;
+}
+.centered {
+  width: 85%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-weight: 500;
+  font-size: 15px;
+}
 .text{
   width: 80%;
   line-height: 25px;
 }
-
 .image{
   width: 70%;
   height: 60px;
 }
-
+.test-list{
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  background-color: #accbb5;
+  padding: 10px;
+  width: 80%;
+  border: none;
+  border-radius: 8px;
+}
+.test-list:focus,
+.test-list:hover{
+  background-color: #8bb096;
+}
+.head{
+  font-size: 25px;
+  margin-bottom: 15px;
+}
 .count{
   font-size: 50px;
   line-height: 45px;
   align-items: center;
   color: #AF7911;
 }
-
+@media (max-width: 995px) {
+  .point-num{
+    max-height: 200px;
+  }
+  .description{
+    height: 70%;
+  }
+}
+@media (max-width: 768px) {
+  .point-num{
+    height: 170px;
+    padding: 0;
+  }
+  .count{
+    font-size: 40px;
+  }
+  .description{
+    height: 90%;
+  }
+  .image{
+    display: none;
+  }
+}
 </style>
---
+++ src/helpers/tests.js
@@ -0,0 +1,9 @@
+import test1 from "./test1.js";
+import test2 from "./test2.js";
+import test3 from "./test3.js";
+
+export default [
+    test1,
+    test2,
+    test3
+]
