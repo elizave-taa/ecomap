a8a51ab156c2eacc440534800b5e70a57ad9a512
ana_sta_r <ana_sta_r@mail.ru>
2023-05-07 22:07:27 +0300

    Push dropdown to right of profile settings

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -48,7 +48,7 @@ export default {
       <router-link class="link" to="/map">Смотреть карту</router-link>
 
       <router-link v-if="$route.name == 'profile'" class="link" to="/profile">
-        <b-dropdown id="dropdown-1" text="Мой профиль" class="btn-exit">
+        <b-dropdown id="dropdown-1" text="Мой профиль" class="btn-exit" right>
           <b-dropdown-item @click="exit()"> Выход </b-dropdown-item>
           <b-dropdown-item @click="exit()"> Редактировать профиль </b-dropdown-item>
         </b-dropdown>
